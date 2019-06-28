47fb747c8ca2dd4249ef064c6b042635dd77f592
Cheongha <2391212dsw>
2019-06-29 00:21:03 +0900

    <SplashActivity> 스플래쉬 액티비티 생성

---
+++ app/src/main/java/com/android/hipart_android/splash/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.android.hipart_android.splash
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.android.hipart_android.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        // 청하
+    }
+}
