db5ec005c3d31a3700a6bb919a602581e340acfd
leesohee <shfksthgml@naver.com>
2019-07-03 02:01:54 +0900

    configure HiPatFragment Layout

---
+++ app/src/main/java/com/android/hipart_android/ui/hipat/AdData.kt
@@ -0,0 +1,5 @@
+package com.android.hipart_android.ui.hipat
+
+data class AdData(
+    var img_url: String
+)
---
+++ app/src/main/java/com/android/hipart_android/ui/hipat/HiPatFragment.kt
@@ -0,0 +1,65 @@
+package com.android.hipart_android.ui.hipat
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.android.hipart_android.R
+import com.android.hipart_android.ui.hipat.adapter.HipatAdRecyclerViewAdapter
+import com.android.hipart_android.ui.hipat.adapter.HipatMainPagerAdapter
+import kotlinx.android.synthetic.main.fragment_hipat.*
+import kotlinx.android.synthetic.main.navigation_category_hipat_port.*
+import org.jetbrains.anko.sdk25.coroutines.onFocusChange
+
+class HiPatFragment : Fragment() {
+
+    lateinit var hipatAdRecyclerViewAdapter : HipatAdRecyclerViewAdapter
+    lateinit var navHipatFragLayout :View
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_hipat, container, false)
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        configureAdRecyclerView()
+        configureMainTab()
+        //setOnTabSelectedListener()
+    }
+
+    private fun configureAdRecyclerView() {
+        val dataList: ArrayList<AdData> = ArrayList()
+
+        hipatAdRecyclerViewAdapter = HipatAdRecyclerViewAdapter(context!!,dataList)
+        rv_hipat_frag_advertisement.adapter= hipatAdRecyclerViewAdapter
+        rv_hipat_frag_advertisement.layoutManager = LinearLayoutManager(context!!,LinearLayoutManager.HORIZONTAL,false)
+    }
+
+    private fun configureMainTab(){
+        vp_hipat_frag_nav.adapter = HipatMainPagerAdapter(fragmentManager!!,5)
+        tl_hipat_frag_pat_nav.setupWithViewPager(vp_hipat_frag_nav)
+
+       navHipatFragLayout = (activity!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
+            .inflate(R.layout.navigation_category_hipat_port,null,false)
+        tl_hipat_frag_pat_nav.getTabAt(0)!!.customView = navHipatFragLayout
+            .findViewById(R.id.rl_hipat_nav_all)
+        tl_hipat_frag_pat_nav.getTabAt(1)!!.customView = navHipatFragLayout
+            .findViewById(R.id.rl_hipat_nav_cpat)
+        tl_hipat_frag_pat_nav.getTabAt(2)!!.customView = navHipatFragLayout
+            .findViewById(R.id.rl_hipat_nav_epat)
+        tl_hipat_frag_pat_nav.getTabAt(3)!!.customView = navHipatFragLayout
+            .findViewById(R.id.rl_hipat_nav_tpat)
+        tl_hipat_frag_pat_nav.getTabAt(4)!!.customView = navHipatFragLayout
+            .findViewById(R.id.rl_hipat_nav_etc)
+
+    }
+
+    private fun setOnTabSelectedListener() {
+    }
+
+}
