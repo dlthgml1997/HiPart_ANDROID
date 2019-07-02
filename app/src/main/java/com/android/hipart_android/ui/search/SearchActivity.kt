f15ced3dd0d5303ade486aaeee57319d9d836af9
vitriol2 <vitriol0919@gmail.com>
2019-07-03 01:57:11 +0900

    search package ing

---
+++ app/src/main/java/com/android/hipart_android/ui/search/SearchActivity.kt
@@ -0,0 +1,13 @@
+package com.android.hipart_android.ui.search
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.android.hipart_android.R
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
---
+++ app/src/main/java/com/android/hipart_android/ui/search/SearchCpatFragment.kt
@@ -0,0 +1,23 @@
+package com.android.hipart_android.ui.search
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.android.hipart_android.R
+
+class SearchCpatFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search_cpat, container, false)
+    }
+
+
+}
