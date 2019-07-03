0644b62fbcc27c84d92b4648d1948256bda62aa4
Tak Hyungmin <milkho11@naver.com>
2019-07-04 06:03:05 +0900

    add home frag pic animation

--- app/src/main/java/com/android/hipart_android/ui/home/HomeFragment.kt
+++ app/src/main/java/com/android/hipart_android/ui/home/HomeFragment.kt
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.android.hipart_android.R
 import com.android.hipart_android.ui.home.adapter.HomeFragAdAdapter
 import com.android.hipart_android.ui.home.adapter.HomeFragHipatAdapter
@@ -14,13 +16,12 @@ import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
-
     private val hipartDataList by lazy {
         ArrayList<HomeFragHipatData>()
     }
 
     private val homeFragHipatAdapter by lazy {
-        HomeFragHipatAdapter(hipartDataList, this@HomeFragment.context)
+        HomeFragHipatAdapter(hipartDataList, this@HomeFragment.context, picAnimListener)
     }
 
     private val adDataList by lazy {
@@ -179,4 +180,42 @@ class HomeFragment : Fragment() {
 
         rv_frag_home_advertising.adapter = homeFragAdAdapter
     }
+
+    fun setAnimPickIcon() {
+        val interpolator = MyBounceInterpolator(0.2, 20.0)
+        val anim: Animation = AnimationUtils.loadAnimation(this@HomeFragment.context, R.anim.expand_anim)
+        anim.interpolator = interpolator
+
+        anim.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                iv_main_act_pick.visibility = View.GONE
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+                iv_main_act_pick.visibility = View.VISIBLE
+            }
+        })
+        iv_main_act_pick.startAnimation(anim)
+    }
+
+    internal inner class MyBounceInterpolator(amplitude: Double, frequency: Double) :
+        android.view.animation.Interpolator {
+        private var mAmplitude = 1.0
+        private var mFrequency = 10.0
+
+        init {
+            mAmplitude = amplitude
+            mFrequency = frequency
+        }
+
+        override fun getInterpolation(time: Float): Float {
+            return (-1.0 * Math.pow(Math.E, -time / mAmplitude) *
+                    Math.cos(mFrequency * time) + 1).toFloat()
+        }
+    }
+
+    private val picAnimListener = View.OnClickListener { setAnimPickIcon() }
 }
