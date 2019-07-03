80a43a967e333e35940e33a5155f1c04d3ef7c87
leesohee <shfksthgml@naver.com>
2019-07-04 06:52:09 +0900

    make NotTpatUploadActivity

--- app/src/main/java/com/android/hipart_android/ui/portfolio/PortFolioFragment.kt
+++ app/src/main/java/com/android/hipart_android/ui/portfolio/PortFolioFragment.kt
@@ -27,7 +27,10 @@ class PortFolioFragment : BottomSheetDialogFragment() {
 
     private fun setonBtnClickListener() {
         btn_port_frag_upload_today_port.setOnClickListener {
+            /* Tpat일 때
             startActivity<TpatUploadActivity>()
+             */
+            startActivity<NotTpatUploadActivity>()
         }
     }
 
