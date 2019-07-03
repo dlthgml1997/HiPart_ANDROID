2bb90197b58525334e7759294a262700cc9167d3
vitriol2 <vitriol0919@gmail.com>
2019-07-03 12:49:35 +0900

    SearchActivity ing

--- app/src/main/java/com/android/hipart_android/ui/search/SearchActivity.kt
+++ app/src/main/java/com/android/hipart_android/ui/search/SearchActivity.kt
@@ -1,13 +1,43 @@
 package com.android.hipart_android.ui.search
 
+import android.opengl.Visibility
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import com.android.hipart_android.R
+import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        setSearch()
+    }
+
+    private fun setSearch() {
+        et_search_act_search.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if(s.toString().length>0) {
+                    ll_search_act_recent_search.visibility = View.GONE
+                    rl_search_act_search_result.visibility = View.VISIBLE
+
+                }else {
+                    ll_search_act_recent_search.visibility = View.VISIBLE
+                    rl_search_act_search_result.visibility = View.GONE
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+        })
     }
 }
--- app/src/main/java/com/android/hipart_android/ui/search/SearchAllFragment.kt
+++ app/src/main/java/com/android/hipart_android/ui/search/SearchAllFragment.kt
@@ -6,8 +6,10 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 
 import com.android.hipart_android.R
+import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchAllFragment : Fragment() {
 
@@ -17,6 +19,30 @@ class SearchAllFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search_all, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        setViewPager()
+
+
+    }
+
+
+    private fun setViewPager() {
+        vp_search_act.adapter = SearchFragmentPagerAdapter(5, childFragmentManager)
+        tl_search_act.setupWithViewPager(vp_search_act)
+
+        val tabSearch = this.layoutInflater.inflate(R.layout.tab_search_frag, null, false)
+
+        tl_search_act.getTabAt(0)!!.customView = tabSearch.findViewById(R.id.iv_tab_search_frag_all) as ImageView
+        tl_search_act.getTabAt(1)!!.customView = tabSearch.findViewById(R.id.iv_tab_search_frag_c) as ImageView
+        tl_search_act.getTabAt(2)!!.customView = tabSearch.findViewById(R.id.iv_tab_search_frag_e) as ImageView
+        tl_search_act.getTabAt(3)!!.customView = tabSearch.findViewById(R.id.iv_tab_search_frag_t) as ImageView
+        tl_search_act.getTabAt(4)!!.customView = tabSearch.findViewById(R.id.iv_tab_search_frag_etc) as ImageView
+
+
     }
 
 
