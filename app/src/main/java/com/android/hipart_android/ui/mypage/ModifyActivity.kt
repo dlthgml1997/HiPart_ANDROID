c2c8a46fbdae8db70d5ce42ae11644c208164806
leesohee <shfksthgml@naver.com>
2019-07-04 23:56:30 +0900

    make ModifyActivity.kt , activity_modify.xml

---
+++ app/src/main/java/com/android/hipart_android/ui/mypage/ModifyActivity.kt
@@ -0,0 +1,13 @@
+package com.android.hipart_android.ui.mypage
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.android.hipart_android.R
+
+class ModifyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modify)
+    }
+}
