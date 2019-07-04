fc70544caebd9cfea8e5447cea8198ac404f2cf6
Tak Hyungmin <milkho11@naver.com>
2019-07-04 21:50:49 +0900

    change bottom layout in MainActivity
    바텀 레이아웃을 바꿨다. 난
    FrameLayout으로.
    왜냐하면 겹치는 PortFolioFragment가
    필요하기 때문이다.

--- app/src/main/java/com/android/hipart_android/ui/main/MainActivity.kt
+++ app/src/main/java/com/android/hipart_android/ui/main/MainActivity.kt
@@ -3,131 +3,138 @@ package com.android.hipart_android.ui.main
 import android.graphics.Color
 import android.os.Bundle
 import android.support.design.widget.BottomSheetDialog
-import android.support.v4.view.ViewPager
-import android.view.LayoutInflater
+import android.support.v4.app.Fragment
 import android.view.View
-import android.widget.RelativeLayout
 import com.android.hipart_android.R
-import com.android.hipart_android.ui.adapter.MainBottomTabAdapter
+import com.android.hipart_android.ui.hipat.HiPatFragment
+import com.android.hipart_android.ui.home.HomeFragment
+import com.android.hipart_android.ui.mypage.fragment.MyPageFragment
+import com.android.hipart_android.ui.portfolio.PortFolioFragment
 import com.android.hipart_android.util.BaseActivity
+import com.android.hipart_android.util.FragmentKind
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.bottom_tab_bar.*
-import org.jetbrains.anko.textColor
 
 /**
-*
-* @author 탁형민
-* @since 2019-07-02 05:46
-**/
+ *
+ * @author 탁형민
+ * @since 2019-07-02 05:46
+ **/
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), View.OnClickListener {
 
-    private val mainBottomTabAdapter : MainBottomTabAdapter by lazy {
-        MainBottomTabAdapter(4, supportFragmentManager)
+    override fun onClick(v: View?) {
+        when (v) {
+            btn_home -> {
+                replaceFragment(R.id.frame_layout_main_act, HomeFragment())
+                setBottomIconChanger(FragmentKind.Home)
+            }
+
+            btn_hi_part -> {
+                replaceFragment(R.id.frame_layout_main_act, HiPatFragment())
+                setBottomIconChanger(FragmentKind.Hipat)
+            }
+
+            btn_portfolio -> {
+                addFragment(R.id.frame_layout_main_act, PortFolioFragment())
+                setBottomIconChanger(FragmentKind.PortFolio)
+            }
+
+            btn_mypage -> {
+                replaceFragment(R.id.frame_layout_main_act, MyPageFragment())
+                setBottomIconChanger(FragmentKind.Mypage)
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        configureMainTabMenu()
-    }
-
-    // TODO
-    // FIXME
-    private fun configureMainTabMenu() {
-        main_fragment_pager.adapter = mainBottomTabAdapter
-        main_fragment_pager.offscreenPageLimit = 4
-        main_bottom_tab_layout.setupWithViewPager(main_fragment_pager)
-        setTabIcon()
-        setMainFragPagerListener()
+        attachHomeFragment()
+        setOnClickListener()
     }
 
 
-    private fun setTabIcon() {
-        val bottomTabView: View = (this.getSystemService(android.content.Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
-            .inflate(R.layout.bottom_tab_bar, null, false)
-
-        main_bottom_tab_layout.getTabAt(0)!!.customView = bottomTabView.findViewById(R.id.btn_home) as RelativeLayout
-        main_bottom_tab_layout.getTabAt(1)!!.customView = bottomTabView.findViewById(R.id.btn_hi_part) as RelativeLayout
-        main_bottom_tab_layout.getTabAt(2)!!.customView = bottomTabView.findViewById(R.id.btn_portfolio) as RelativeLayout
-        main_bottom_tab_layout.getTabAt(3)!!.customView = bottomTabView.findViewById(R.id.btn_mypage) as RelativeLayout
+    private fun attachHomeFragment() {
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.frame_layout_main_act, HomeFragment())
+            .commit()
     }
 
-    private fun setMainFragPagerListener(){
-        main_fragment_pager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener{
-            override fun onPageScrollStateChanged(p0: Int) {
+    private fun replaceFragment(frameLayoutId: Int, fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(frameLayoutId, fragment)
+            .commit()
+    }
 
+    private fun addFragment(frameLayoutId: Int, fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .add(frameLayoutId, fragment)
+            .commit()
+    }
+
+    private fun setOnClickListener() {
+        btn_home.setOnClickListener(this)
+        btn_hi_part.setOnClickListener(this)
+        btn_portfolio.setOnClickListener(this)
+        btn_mypage.setOnClickListener(this)
+    }
+
+    private fun setBottomIconChanger(fragKind: FragmentKind) {
+
+        when (fragKind) {
+            FragmentKind.Home -> {
+                iv_home.setImageResource(R.drawable.main_tb_home_on_icon)
+                iv_hi_part.isSelected = false
+                iv_portfolio.isSelected = false
+                iv_mypage.isSelected = false
+
+                tv_home.setTextColor(Color.parseColor("#7947fd"))
+                tv_hipart.setTextColor(Color.parseColor("#838383"))
+                tv_portfolio.setTextColor(Color.parseColor("#838383"))
+                tv_mypage.setTextColor(Color.parseColor("#838383"))
             }
+            FragmentKind.Hipat -> {
+                iv_home.setImageResource(R.drawable.main_tb_home_off_icon)
+                iv_hi_part.isSelected = true
+                iv_portfolio.isSelected = false
+                iv_mypage.isSelected = false
 
-            override fun onPageScrolled(p0: Int, p1: Float, p2: Int) {
-                when (p0) {
-                    0 -> {
-                        iv_home.setImageResource(R.drawable.main_tb_home_on_icon)
-                        tv_home.textColor = Color.parseColor("#7947fd")
-
-                        iv_hi_part.setImageResource(R.drawable.main_tb_hipot_off_icon)
-                        tv_hipart.textColor = Color.parseColor("#838383")
-
-                        iv_portfolio.setImageResource(R.drawable.main_tb_pofol_off_icon)
-                        tv_portfolio.textColor = Color.parseColor("#838383")
-
-                        iv_mypage.setImageResource(R.drawable.main_tb_mypg_off_icon)
-                        tv_mypage.textColor = Color.parseColor("#838383")
-                    }
-                    1 -> {
-                        iv_home.setImageResource(R.drawable.main_tb_home_on_icon)
-                        tv_home.textColor = Color.parseColor("#838383")
-
-                        iv_hi_part.setImageResource(R.drawable.main_tb_hipot_off_icon)
-                        tv_hipart.textColor = Color.parseColor("#7947fd")
-
-                        iv_portfolio.setImageResource(R.drawable.main_tb_pofol_off_icon)
-                        tv_portfolio.textColor = Color.parseColor("#838383")
-
-                        iv_mypage.setImageResource(R.drawable.main_tb_mypg_off_icon)
-                        tv_mypage.textColor = Color.parseColor("#838383")
-
-                    }
-                    2 -> {
-
-                        iv_home.setImageResource(R.drawable.main_tb_home_on_icon)
-                        tv_home.textColor = Color.parseColor("#838383")
-
-                        iv_hi_part.setImageResource(R.drawable.main_tb_hipot_off_icon)
-                        tv_hipart.textColor = Color.parseColor("#838383")
-
-                        iv_portfolio.setImageResource(R.drawable.main_tb_pofol_off_icon)
-                        tv_portfolio.textColor = Color.parseColor("#7947fd")
-
-                        iv_mypage.setImageResource(R.drawable.main_tb_mypg_off_icon)
-                        tv_mypage.textColor = Color.parseColor("#838383")
-
-//                        setBottomSheetDialog()
-
-                    }
-                    3 -> {
-                        iv_home.setImageResource(R.drawable.main_tb_home_on_icon)
-                        tv_home.textColor = Color.parseColor("#838383")
-
-                        iv_hi_part.setImageResource(R.drawable.main_tb_hipot_off_icon)
-                        tv_hipart.textColor = Color.parseColor("#838383")
-
-                        iv_portfolio.setImageResource(R.drawable.main_tb_pofol_off_icon)
-                        tv_portfolio.textColor = Color.parseColor("#838383")
-
-                        iv_mypage.setImageResource(R.drawable.main_tb_mypg_off_icon)
-                        tv_mypage.textColor = Color.parseColor("#7947fd")
-                    }
-                }
+                tv_home.setTextColor(Color.parseColor("#838383"))
+                tv_hipart.setTextColor(Color.parseColor("#7947fd"))
+                tv_portfolio.setTextColor(Color.parseColor("#838383"))
+                tv_mypage.setTextColor(Color.parseColor("#838383"))
             }
+            FragmentKind.PortFolio -> {
+                iv_home.setImageResource(R.drawable.main_tb_home_off_icon)
+                iv_hi_part.isSelected = false
+                iv_portfolio.isSelected = true
+                iv_mypage.isSelected = false
 
-            override fun onPageSelected(p0: Int) {
+                tv_home.setTextColor(Color.parseColor("#838383"))
+                tv_hipart.setTextColor(Color.parseColor("#838383"))
+                tv_portfolio.setTextColor(Color.parseColor("#7947fd"))
+                tv_mypage.setTextColor(Color.parseColor("#838383"))
             }
-        })
+            FragmentKind.Mypage -> {
+                iv_home.setImageResource(R.drawable.main_tb_home_off_icon)
+                iv_hi_part.isSelected = false
+                iv_portfolio.isSelected = false
+                iv_mypage.isSelected = true
+
+                tv_home.setTextColor(Color.parseColor("#838383"))
+                tv_hipart.setTextColor(Color.parseColor("#838383"))
+                tv_portfolio.setTextColor(Color.parseColor("#838383"))
+                tv_mypage.setTextColor(Color.parseColor("#7947fd"))
+            }
+        }
+
     }
 
     private fun setBottomSheetDialog() {
-        val view = layoutInflater.inflate(R.layout.fragment_portfolio_bottom_sheet_dialog,null)
+        val view = layoutInflater.inflate(R.layout.fragment_portfolio_bottom_sheet_dialog, null)
         val dialog = BottomSheetDialog(this)
         dialog.setContentView(view)
         dialog.show()
