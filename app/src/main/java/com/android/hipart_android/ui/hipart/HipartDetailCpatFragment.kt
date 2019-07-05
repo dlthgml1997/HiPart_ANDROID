cc723030c6cffd25b977d990a6ed7cfa80191067
vitriol2 <vitriol0919@gmail.com>
2019-07-05 16:44:16 +0900

    Revert "contact function ing"
    This reverts commit c8df386d093e783400e90b2dfb29e888f5fc01ae.

--- app/src/main/java/com/android/hipart_android/ui/hipart/HipartDetailCpatFragment.kt
+++ app/src/main/java/com/android/hipart_android/ui/hipart/HipartDetailCpatFragment.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.android.hipart_android.R
-import kotlinx.android.synthetic.main.fragment_hipart_detail_cpat.*
 
 class HipartDetailCpatFragment : Fragment() {
 
@@ -22,11 +21,6 @@ class HipartDetailCpatFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
-        btn_hip_det_c_frag_contact.setOnClickListener {
-            val contactDialog : ContactDialogFragment = ContactDialogFragment()
-            contactDialog.show(childFragmentManager, "contact Dialog")
-        }
 
 
     }
