30aa425311177b172425c5a1b1be9a77438c25d3
Tak Hyungmin <milkho11@naver.com>
2019-07-06 18:23:43 +0900

    create SharedPreferenceController

---
+++ app/src/main/java/com/android/hipart_android/util/SharedPreferenceController.kt
@@ -0,0 +1,47 @@
+package com.android.hipart_android.util
+
+import android.content.Context
+
+
+/**
+ * Created by TakHyeongMin on 2019-07-06.
+ */
+object SharedPreferenceController {
+
+    // 변수부
+    private val USER_NAME = "MYKEY"
+    private val kakaoKey = "kakao_Key"
+    //807465239
+    private val my_id = "my_id"
+
+    fun setAuthorization(context: Context, authorization : String){
+        val pref = context.getSharedPreferences(USER_NAME, Context.MODE_PRIVATE) //현재 내 기기에서만 볼수 있는 데이터
+        val editor = pref.edit()
+        editor.putString(kakaoKey, authorization)
+        editor.commit()
+    }
+
+    fun getAuthorization(context: Context) : String {
+        val pref = context.getSharedPreferences(USER_NAME, Context.MODE_PRIVATE) //현재 내 기기에서만 볼수 있는 데이터
+        return pref.getString(kakaoKey, "")
+    }
+
+    fun setMyId(context: Context, id : String){
+        val pref = context.getSharedPreferences(USER_NAME, Context.MODE_PRIVATE) //현재 내 기기에서만 볼수 있는 데이터
+        val editor = pref.edit()
+        editor.putString(my_id, id)
+        editor.commit()
+    }
+
+    fun getMyId(context: Context) : String {
+        val pref = context.getSharedPreferences(USER_NAME, Context.MODE_PRIVATE) //현재 내 기기에서만 볼수 있는 데이터
+        return pref.getString(my_id, "")
+    }
+
+    fun clearSPC(context: Context){
+        val pref = context.getSharedPreferences(USER_NAME, Context.MODE_PRIVATE) //현재 내 기기에서만 볼수 있는 데이터
+        val editor = pref.edit()
+        editor.clear()
+        editor.commit()
+    }
+}
