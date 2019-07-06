c340b17c941bbd7a464c2c3d82fb06debcdfaa40
Tak Hyungmin <milkho11@naver.com>
2019-07-06 15:56:16 +0900

    modify package route

--- app/src/main/java/com/android/hipart_android/ui/main/adapter/MainBottomTabAdapter.kt
+++ app/src/main/java/com/android/hipart_android/ui/main/adapter/MainBottomTabAdapter.kt
@@ -1,4 +1,4 @@
-package com.android.hipart_android.ui.adapter
+package com.android.hipart_android.ui.main.adapter
 
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
--- app/src/main/java/com/android/hipart_android/ui/main/adapter/MainViewPager.kt
+++ app/src/main/java/com/android/hipart_android/ui/main/adapter/MainViewPager.kt
@@ -1,4 +1,4 @@
-package com.android.hipart_android.ui.adapter
+package com.android.hipart_android.ui.main.adapter
 
 import android.content.Context
 import android.support.v4.view.ViewPager
