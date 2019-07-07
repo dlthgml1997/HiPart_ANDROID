68cdfc33ce0217e52ca6d44285313da48691d781
Tak Hyungmin <milkho11@naver.com>
2019-07-07 17:08:03 +0900

    Add ApplicationController.kt
    add NetworkService.kt

---
+++ app/src/main/java/com/android/hipart_android/network/NetworkService.kt
@@ -0,0 +1,18 @@
+package com.android.hipart_android.network
+
+
+/**
+ * Created by TakHyeongMin on 2019-07-07.
+ */
+interface NetworkService {
+    // 도경 라인 ( 각 뷰 별로 주석으로 나눠주세요. )
+
+    // 청하 라인 ( 각 뷰 별로 주석으로 나눠주세요. )
+
+    // 소희 라인 ( 각 뷰 별로 주석으로 나눠주세요. )
+
+    // 지원 라인 ( 각 뷰 별로 주석으로 나눠주세요. )
+
+    // 형민 라인 ( 각 뷰 별로 주석으로 나눠주세요. )
+
+}
