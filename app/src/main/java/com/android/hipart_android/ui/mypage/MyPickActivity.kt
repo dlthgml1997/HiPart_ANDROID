96c1e0c4634156f4e543af13e2ac53cec6a8cff1
Tak Hyungmin <milkho11@naver.com>
2019-07-07 18:07:46 +0900

    add animatoin in MainAct, MyPickAct

--- app/src/main/java/com/android/hipart_android/ui/mypage/MyPickActivity.kt
+++ app/src/main/java/com/android/hipart_android/ui/mypage/MyPickActivity.kt
@@ -1,15 +1,26 @@
 package com.android.hipart_android.ui.mypage
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.LinearLayout
 import com.android.hipart_android.R
 import com.android.hipart_android.ui.hipat.adapter.PortFolioRecyclerViewAdapter
 import com.android.hipart_android.ui.hipat.data.PortFolioData
 import kotlinx.android.synthetic.main.activity_mypick.*
+import kotlinx.android.synthetic.main.toolbar_mypick.*
 
-class MyPickActivity : AppCompatActivity() {
+class MyPickActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(v: View?) {
+        when(v){
+            btn_my_pick_back -> {
+                finish()
+            }
+        }
+    }
 
     val dataList by lazy {
         ArrayList<PortFolioData>()
@@ -22,6 +33,7 @@ class MyPickActivity : AppCompatActivity() {
         setContentView(R.layout.activity_mypick)
 
         configureRecyclerView()
+        setOnClickListener()
     }
 
     private fun configureRecyclerView() {
@@ -56,8 +68,48 @@ class MyPickActivity : AppCompatActivity() {
             )
         )
 
-        portFolioRecyclerViewAdapter= PortFolioRecyclerViewAdapter(this,dataList)
+        portFolioRecyclerViewAdapter= PortFolioRecyclerViewAdapter(this,dataList, false)
         rv_mypick_act_port.adapter = portFolioRecyclerViewAdapter
         rv_mypick_act_port.layoutManager = LinearLayoutManager(this,LinearLayout.VERTICAL,false)
+    }
+
+    fun setAnimPickIcon() {
+        val interpolator = MyBounceInterpolator(0.2, 20.0)
+        val anim: Animation = AnimationUtils.loadAnimation(this@MyPickActivity, R.anim.expand_anim)
+        anim.interpolator = interpolator
+
+        anim.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                rl_mypick_act_anim.visibility = View.GONE
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+                rl_mypick_act_anim.visibility = View.VISIBLE
+            }
+        })
+        rl_mypick_act_anim.startAnimation(anim)
+    }
+
+    internal inner class MyBounceInterpolator(amplitude: Double, frequency: Double) :
+        android.view.animation.Interpolator {
+        private var mAmplitude = 1.0
+        private var mFrequency = 10.0
+
+        init {
+            mAmplitude = amplitude
+            mFrequency = frequency
+        }
+
+        override fun getInterpolation(time: Float): Float {
+            return (-1.0 * Math.pow(Math.E, -time / mAmplitude) *
+                    Math.cos(mFrequency * time) + 1).toFloat()
+        }
+    }
+
+    private fun setOnClickListener() {
+        btn_my_pick_back.setOnClickListener(this)
     }
 }
