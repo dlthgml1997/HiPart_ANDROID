5830269966cb2170e02f1c1650b8d26a2cf5334d
Tak Hyungmin <milkho11@naver.com>
2019-07-09 04:46:49 +0900

    다이얼로그 수정

--- app/src/main/java/com/android/hipart_android/ui/mypage/fragment/MyPageFragment.kt
+++ app/src/main/java/com/android/hipart_android/ui/mypage/fragment/MyPageFragment.kt
@@ -1,18 +1,15 @@
 package com.android.hipart_android.ui.mypage.fragment
 
-import android.content.Intent
 import android.os.Bundle
-import android.support.v4.app.DialogFragment
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.android.hipart_android.R
-import com.android.hipart_android.ui.mypage.ModifyActivity
+import com.android.hipart_android.ui.modifyprofile.ModifyActivity
 import com.android.hipart_android.ui.mypage.MyPickActivity
 import com.android.hipart_android.ui.mypage.dialog.HifiveDialog
 import com.android.hipart_android.ui.mypage.dialog.QuestionDialog
-import kotlinx.android.synthetic.main.dialog_mypage_hifive.*
 import kotlinx.android.synthetic.main.fragment_mypage.*
 import org.jetbrains.anko.support.v4.startActivity
 
