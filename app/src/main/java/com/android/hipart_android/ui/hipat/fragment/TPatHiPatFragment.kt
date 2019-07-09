6459b62037b12c7d2c81ef0c0c4c1485bff3092a
Tak Hyungmin <milkho11@naver.com>
2019-07-10 05:04:51 +0900

    프로필 보기 API 완성

--- app/src/main/java/com/android/hipart_android/ui/hipat/fragment/TPatHiPatFragment.kt
+++ app/src/main/java/com/android/hipart_android/ui/hipat/fragment/TPatHiPatFragment.kt
@@ -3,14 +3,21 @@ package com.android.hipart_android.ui.hipat.fragment
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.android.hipart_android.R
+import com.android.hipart_android.network.ApplicationController
+import com.android.hipart_android.network.NetworkService
 import com.android.hipart_android.ui.hipat.adapter.PortFolioRecyclerViewAdapter
-import kotlinx.android.synthetic.main.fragment_hipat_tpat.*
+import com.android.hipart_android.ui.hipat.data.GetProfileLookUpResponse
 import com.android.hipart_android.ui.mypick.data.GetMyPickData
+import com.android.hipart_android.util.SharedPreferenceController
+import kotlinx.android.synthetic.main.fragment_hipat_tpat.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class TPatHiPatFragment : Fragment() {
 
@@ -41,5 +48,36 @@ class TPatHiPatFragment : Fragment() {
         portFolioRecyclerViewAdapter = PortFolioRecyclerViewAdapter(context!!, dataList,true)
         rv_hipat_tpat_frag.adapter = portFolioRecyclerViewAdapter
         rv_hipat_tpat_frag.layoutManager = LinearLayoutManager(context!!, LinearLayoutManager.VERTICAL, false)
+        getProfileLookUp(3)
+    }
+
+    fun getProfileLookUp(flag : Int){
+        var networkService: NetworkService = ApplicationController.instance.networkService
+
+        val getProfileLookUp = networkService.getProfileLookUp(
+            SharedPreferenceController.getAuthorization(this@TPatHiPatFragment.context!!),
+            flag
+        )
+
+        getProfileLookUp.enqueue(object: Callback<GetProfileLookUpResponse> {
+            override fun onFailure(call: Call<GetProfileLookUpResponse>, t: Throwable) {
+                Log.e("All HighPat Frag Err", Log.getStackTraceString(t))
+            }
+
+            override fun onResponse(
+                call: Call<GetProfileLookUpResponse>,
+                response: Response<GetProfileLookUpResponse>
+            ) {
+                response
+                    ?.takeIf { it.isSuccessful }
+                    ?.body()
+                    ?.takeIf { it.message == "조회 성공" }
+                    ?.data
+                    ?.let {
+                        portFolioRecyclerViewAdapter.dataList = it
+                        portFolioRecyclerViewAdapter.notifyDataSetChanged()
+                    }
+            }
+        })
     }
 }
