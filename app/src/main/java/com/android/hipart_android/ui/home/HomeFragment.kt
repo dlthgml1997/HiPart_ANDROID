c35c79737fdafe6433e69ca46a6da005a1418f22
Tak Hyungmin <milkho11@naver.com>
2019-07-09 22:11:59 +0900

    attach homefrag recycelerView

--- app/src/main/java/com/android/hipart_android/ui/home/HomeFragment.kt
+++ app/src/main/java/com/android/hipart_android/ui/home/HomeFragment.kt
@@ -7,16 +7,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.android.hipart_android.R
+import com.android.hipart_android.network.ApplicationController
 import com.android.hipart_android.ui.hipat.HiPatFragment
 import com.android.hipart_android.ui.home.adapter.HomeFragAdAdapter
 import com.android.hipart_android.ui.home.adapter.HomeFragHipatAdapter
 import com.android.hipart_android.ui.home.data.HomeFragAdData
-import com.android.hipart_android.ui.home.data.HomeFragHipatData
+import com.android.hipart_android.ui.home.data.get.GetCustomRecommendResponse
+import com.android.hipart_android.ui.home.data.get.ResData
 import com.android.hipart_android.ui.main.MainActivity
 import com.android.hipart_android.ui.notification.NotificationActivity
 import com.android.hipart_android.ui.search.SearchActivity
+import com.android.hipart_android.util.SharedPreferenceController
 import kotlinx.android.synthetic.main.fragment_home.*
 import org.jetbrains.anko.support.v4.startActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HomeFragment : Fragment(), View.OnClickListener {
     override fun onClick(v: View?) {
@@ -52,9 +58,7 @@ class HomeFragment : Fragment(), View.OnClickListener {
         }
     }
 
-    private val hipartDataList by lazy {
-        ArrayList<HomeFragHipatData>()
-    }
+    lateinit var hipartDataList : ArrayList<ResData>
 
     private val homeFragHipatAdapter by lazy {
         HomeFragHipatAdapter(hipartDataList, this@HomeFragment.context)
@@ -74,95 +78,19 @@ class HomeFragment : Fragment(), View.OnClickListener {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        hipartDataList = ArrayList<ResData>()
+        getCustomRecommend()
         setVpAdapter()
         setOnClickListener()
+
     }
 
     private fun setVpAdapter() {
-        setPatVpAdapter()
         setAdVpAdapter()
 
     }
 
     private fun setPatVpAdapter() {
-        hipartDataList.add(
-            HomeFragHipatData(
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa"
-            )
-        )
-
-        hipartDataList.add(
-            HomeFragHipatData(
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa"
-            )
-        )
-
-        hipartDataList.add(
-            HomeFragHipatData(
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa"
-            )
-        )
-
-        hipartDataList.add(
-            HomeFragHipatData(
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa"
-            )
-        )
-
-        hipartDataList.add(
-            HomeFragHipatData(
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa"
-            )
-        )
-
-        hipartDataList.add(
-            HomeFragHipatData(
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa",
-                "aa"
-            )
-        )
-
         vp_frag_home_recommend_hipat.adapter = homeFragHipatAdapter
     }
 
@@ -226,4 +154,34 @@ class HomeFragment : Fragment(), View.OnClickListener {
         btn_frag_home_t_pat.setOnClickListener(this)
         btn_frag_home_etc.setOnClickListener(this)
     }
+
+    private fun getCustomRecommend(){
+        val networkService = ApplicationController.instance.networkService
+
+        val getCustomRecommend = networkService.customRecommend(
+            SharedPreferenceController.getAuthorization(this@HomeFragment.context!!)
+        )
+
+        getCustomRecommend.enqueue(object: Callback<GetCustomRecommendResponse>{
+            override fun onFailure(call: Call<GetCustomRecommendResponse>, t: Throwable) {
+                Log.e("Home Frag Err", Log.getStackTraceString(t))
+            }
+
+            override fun onResponse(
+                call: Call<GetCustomRecommendResponse>,
+                response: Response<GetCustomRecommendResponse>
+            ) {
+                Log.e("ㅇㄴ!!!!", response.body()!!.data.resData.toString())
+                response
+                    ?.takeIf { it.isSuccessful }
+                    ?.body()
+                    ?.data
+                    ?.let{
+                        hipartDataList = it.resData
+                        setPatVpAdapter()
+                    }
+            }
+        })
+    }
+
 }
