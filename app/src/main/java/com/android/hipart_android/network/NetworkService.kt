6e0501cb529e8632ede28fabc409c6fffa1f0c5e
Tak Hyungmin <milkho11@naver.com>
2019-07-09 22:20:09 +0900

    NetworkService 소희, 청하 추가

--- app/src/main/java/com/android/hipart_android/network/NetworkService.kt
+++ app/src/main/java/com/android/hipart_android/network/NetworkService.kt
@@ -6,19 +6,20 @@ import com.android.hipart_android.ui.home.data.post.PickResponse
 import com.android.hipart_android.ui.login.data.PostLoginRequest
 import com.android.hipart_android.ui.login.data.PostLoginResponse
 import com.android.hipart_android.ui.modifyportfolio.get.GetModifyPortFolioResponseCpat
-import com.android.hipart_android.ui.modifyportfolio.get.GetModifyPortFolioResponseEpat
+import com.android.hipart_android.ui.modifyportfolio.get.GetModifyPortFolioResponseEpatAndEtc
+import com.android.hipart_android.ui.modifyportfolio.get.GetModifyPortFolioResponseTpat
 import com.android.hipart_android.ui.modifyprofile.data.get.GetModifyProfileResponse
 import com.android.hipart_android.ui.modifyprofile.data.put.ModifyProfileResponse
+import com.android.hipart_android.ui.mypage.data.GetMyPickResponse
 import com.android.hipart_android.ui.mypage.data.PostManToManQuestionRequest
 import com.android.hipart_android.ui.mypage.get.GetMypageResponse
+import com.android.hipart_android.ui.notification.get.GetNotificationResponse
 import com.android.hipart_android.ui.signup.data.GetDuplicateFlagResponse
 import com.android.hipart_android.ui.signup.data.PostSignUpResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.*
-
-
 
 
 /**
@@ -34,21 +35,27 @@ interface NetworkService {
     ): Call<GetMyPickResponse>
 
     // 청하 라인 ( 각 뷰 별로 주석으로 나눠주세요. )
-    // 로그인-청하
+//    // 로그인-청하
+//    @POST("auth/signin")
+//    fun postLoginResponse(
+//        @Header("Content-Type") content_type: String,
+//        @Body body: PostLoginRequest
+//    ): Call<PostLoginResponse>
+
     @POST("auth/signin")
     fun postLoginResponse(
         @Header("Content-Type") content_type: String,
         @Body body: PostLoginRequest
     ): Call<PostLoginResponse>
 
-//    /**
-//     * 알림 조회
-//     */
-//    @GET("mypage/info")
-//    fun getNotificationResponse(
-//        @Header("Content-Type") content_type: String,
-//        @Header("token") token: String
-//    ): Call<GetNotificationResponse>
+    /**
+     * 알림 조회
+     */
+    @GET("main/notification")
+    fun getNotificationResponse(
+        @Header("Content-Type") content_type: String,
+        @Header("token") token: String
+    ): Call<GetNotificationResponse>
 
     /**
      * 마이 페이지
@@ -58,6 +65,24 @@ interface NetworkService {
         @Header("Content-Type") content_type: String,
         @Header("token") token: String
     ): Call<GetMypageResponse>
+
+//    /**
+//     * 알림 조회
+//     */
+//    @GET("mypage/info")
+//    fun getNotificationResponse(
+//        @Header("Content-Type") content_type: String,
+//        @Header("token") token: String
+//    ): Call<GetNotificationResponse>
+//
+//    /**
+//     * 마이 페이지
+//     */
+//    @GET("mypage/info")
+//    fun getMypageResponse(
+//        @Header("Content-Type") content_type: String,
+//        @Header("token") token: String
+//    ): Call<GetMypageResponse>
 
     // 소희 라인 ( 각 뷰 별로 주석으로 나눠주세요. )
 //    /**
@@ -69,6 +94,29 @@ interface NetworkService {
 //        @Header("token") token: String
 //    ): Call<GetModifyPortFolioResponse>
 
+    //    /**
+//     * 포트폴리오 수정 페이지 로딩 - 에디터
+//     */
+//    @GET("portfolio/detail/editor")
+//    fun getModifyPortFolioResponseEpat(
+//        @Header("Content-Type") content_type: String,
+//        @Header("token") token: String
+//    ): Call<GetModifyPortFolioResponseEpat>
+//
+//    /**
+//     * 포트폴리오 수정 페이지 로딩 - 크리에이터
+//     */
+//    @GET("portfolio/detail/creator")
+//    fun getModifyPortFolioResponseCpat(
+//        @Header("Content-Type") content_type: String,
+//        @Header("token") token: String
+//    ): Call<GetModifyPortFolioResponseCpat>
+    @GET("portfolio/detail/creator")
+    fun getModifyPortFolioResponseCpat(
+        @Header("Content-Type") content_type: String,
+        @Header("token") token: String
+    ): Call<GetModifyPortFolioResponseCpat>
+
     /**
      * 포트폴리오 수정 페이지 로딩 - 에디터
      */
@@ -76,16 +124,27 @@ interface NetworkService {
     fun getModifyPortFolioResponseEpat(
         @Header("Content-Type") content_type: String,
         @Header("token") token: String
-    ): Call<GetModifyPortFolioResponseEpat>
+    ): Call<GetModifyPortFolioResponseEpatAndEtc>
 
     /**
-     * 포트폴리오 수정 페이지 로딩 - 크리에이터
+     * 포트폴리오 수정 페이지 로딩 - 트랜슬레이터
+     * @header token
      */
-    @GET("portfolio/detail/creator")
-    fun getModifyPortFolioResponseCpat(
+    @GET("portfolio/detail/translator")
+    fun getModifyPortFolioResponseTpat(
         @Header("Content-Type") content_type: String,
         @Header("token") token: String
-    ): Call<GetModifyPortFolioResponseCpat>
+    ): Call<GetModifyPortFolioResponseTpat>
+
+    /**
+     * 포트폴리오 수정 페이지 로딩 - 기타
+     * @header token
+     */
+    @GET("portfolio/detail/etc")
+    fun getModifyPortFolioResponseEtc(
+        @Header("Content-Type") content_type: String,
+        @Header("token") token: String
+    ): Call<GetModifyPortFolioResponseEpatAndEtc>
     // 지원 라인 ( 각 뷰 별로 주석으로 나눠주세요. )
 
     // 형민 라인 ( 각 뷰 별로 주석으로 나눠주세요. )
@@ -100,9 +159,9 @@ interface NetworkService {
         @Part("user_email") userEmail: RequestBody,
         @Part("user_nickname") userNickname: RequestBody,
         @Part user_img: MultipartBody.Part?,
-        @Part("user_pw") userPw : RequestBody,
-        @Part("user_number") userNumber : RequestBody,
-        @Part("user_type") userType : Int
+        @Part("user_pw") userPw: RequestBody,
+        @Part("user_number") userNumber: RequestBody,
+        @Part("user_type") userType: Int
     ): Call<PostSignUpResponse>
 
     /**
@@ -117,9 +176,9 @@ interface NetworkService {
      */
     @GET("auth/duplicated/{flag}/{input}")
     fun getDuplicateFlag(
-        @Path("flag") flag : Int,
-        @Path("input") input : String
-    ) : Call<GetDuplicateFlagResponse>
+        @Path("flag") flag: Int,
+        @Path("input") input: String
+    ): Call<GetDuplicateFlagResponse>
 
     /**
      * 픽추가
@@ -130,7 +189,7 @@ interface NetworkService {
     fun addPick(
         @Header("token") token: String,
         @Body pickDTO: PickDTO
-    ) : Call<PickResponse>
+    ): Call<PickResponse>
 
     /**
      * 픽 삭제
@@ -151,8 +210,8 @@ interface NetworkService {
     @POST("mypage/question")
     fun postManToManQusetion(
         @Header("Token") token: String,
-        @Body postManToManQuestionRequest : PostManToManQuestionRequest
-    ) : Call<PickResponse>
+        @Body postManToManQuestionRequest: PostManToManQuestionRequest
+    ): Call<PickResponse>
 
     /**
      * 회원정보 조회
@@ -162,7 +221,7 @@ interface NetworkService {
     @GET("mypage/modify")
     fun getProfile(
         @Header("Token") token: String
-    ) : Call<GetModifyProfileResponse>
+    ): Call<GetModifyProfileResponse>
 
     /**
      * 회원정보 변경
@@ -174,11 +233,11 @@ interface NetworkService {
         @Header("Token") token: String,
         @Part user_img: MultipartBody.Part?,
         @Part("user_nickname") userNickname: RequestBody,
-        @Part("user_number") userNumber : RequestBody,
-        @Part("user_pw") userPw : RequestBody,
+        @Part("user_number") userNumber: RequestBody,
+        @Part("user_pw") userPw: RequestBody,
         @Part("new_pw") newPw: RequestBody,
-        @Part("user_type") userType : Int
-    ) : Call<ModifyProfileResponse>
+        @Part("user_type") userType: Int
+    ): Call<ModifyProfileResponse>
 
     /**
      * 맞춤추천
@@ -187,7 +246,7 @@ interface NetworkService {
     @GET("profile/recommend")
     fun customRecommend(
         @Header("token") token: String
-    ) : Call<GetCustomRecommendResponse>
+    ): Call<GetCustomRecommendResponse>
 
 
 }
