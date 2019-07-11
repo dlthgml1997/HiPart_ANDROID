825839d4e9f6d371647422bcb3c92e9a8430d439
Tak Hyungmin <milkho11@naver.com>
2019-07-12 06:27:58 +0900

    splash 추가

--- app/src/main/java/com/android/hipart_android/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/android/hipart_android/ui/splash/SplashActivity.kt
@@ -1,14 +1,34 @@
 package com.android.hipart_android.ui.splash
 
+import android.animation.Animator
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.android.hipart_android.R
+import com.android.hipart_android.ui.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+import org.jetbrains.anko.startActivity
+
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        // 청하
+
+        animation_act_splash.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(animation: Animator?) {
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                startActivity<LoginActivity>()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+            }
+        })
+        animation_act_splash.playAnimation()
     }
 }
