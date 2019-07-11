9866c5180ba7871f45cb65ac6398185cf2e7b112
Tak Hyungmin <milkho11@naver.com>
2019-07-11 17:12:30 +0900

    set HiPatDetailAct Intent val

--- app/src/main/java/com/android/hipart_android/ui/hipart/HipartDetailActivity.kt
+++ app/src/main/java/com/android/hipart_android/ui/hipart/HipartDetailActivity.kt
@@ -24,6 +24,8 @@ class HipartDetailActivity : BaseActivity() {
     lateinit var userDetailData: UserDetailEEtcData
     lateinit var userDetailTData: UserDetailTData
 
+    private val userID = ""
+    private val userNickName = ""
 
     private val networkService = ApplicationController.instance.networkService
 
@@ -32,7 +34,7 @@ class HipartDetailActivity : BaseActivity() {
         setContentView(R.layout.activity_hipart_detail)
 
         //여기 숫자에 인텐트로 받아온 유저의 user_nickname, user_type
-        filterUser("cuteyang", 1)
+        filterUser(intent.getStringExtra("user_nickname"), intent.getIntExtra("user_type", 0))
 
     }
 
