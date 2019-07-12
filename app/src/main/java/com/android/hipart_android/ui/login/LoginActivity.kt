cb3131f1cca6fb3b747ae7c9c2484c6335fab21f
Tak Hyungmin <milkho11@naver.com>
2019-07-12 22:06:16 +0900

    체험하기

--- app/src/main/java/com/android/hipart_android/ui/login/LoginActivity.kt
+++ app/src/main/java/com/android/hipart_android/ui/login/LoginActivity.kt
@@ -2,7 +2,9 @@ package com.android.hipart_android.ui.login
 
 import android.os.Bundle
 import android.text.Editable
+import android.text.SpannableString
 import android.text.TextWatcher
+import android.text.style.UnderlineSpan
 import android.util.Log
 import android.view.View
 import com.android.hipart_android.R
@@ -15,6 +17,7 @@ import com.android.hipart_android.ui.login.data.RefreshTokenResponse
 import com.android.hipart_android.ui.main.MainActivity
 import com.android.hipart_android.ui.signup.SignupActivity
 import com.android.hipart_android.util.BaseActivity
+import com.android.hipart_android.util.OnSingleClickListener
 import com.android.hipart_android.util.SharedPreferenceController
 import kotlinx.android.synthetic.main.activity_login.*
 import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEvent
@@ -25,12 +28,13 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+
 class LoginActivity : BaseActivity(), KeyboardVisibilityEventListener {
     override fun onVisibilityChanged(isOpen: Boolean) {
-        if(isOpen) {
+        if (isOpen) {
             ll_login_act_sign_in.visibility = View.GONE
             sv_login_act.scrollTo(0, sv_login_act.bottom)
-        }else{
+        } else {
 
             ll_login_act_sign_in.visibility = View.VISIBLE
             sv_login_act.scrollTo(0, sv_login_act.top)
@@ -39,7 +43,7 @@ class LoginActivity : BaseActivity(), KeyboardVisibilityEventListener {
 
     val REQUEST_CODE_LOGIN_ACTIVITY = 1000
 
-    val networkService: NetworkService by lazy{
+    val networkService: NetworkService by lazy {
         ApplicationController.instance.networkService
     }
 
@@ -47,14 +51,12 @@ class LoginActivity : BaseActivity(), KeyboardVisibilityEventListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        KeyboardVisibilityEvent.setEventListener(this, this)
-
+        setUnderlineText()
         setAutoLogin()
         setOnFocusChangeListener()
         setTextChangedListenerEmail()
         setTextChangedListenerPassword()
         setOnBtnClickListener()
-        refreshToken("chI2McxQf89Y9krDcXCKBkEnAkzQ1Yc7L56voJc7pa20vC8LDnbNZymPsSfex6zYtnSfc93GKCNqbXjDujkU5cvaAJRAoUwR6Gzr62XRxEQZ6A5NAlxEpWN4sRFpn5VlMWD3zAC7HJStjXtLjhPr8GOfW77W4KiyRiHGU13CWurDWHHPeaIzbd9Wqzg1pqFq2uwNkqF99smCCuIgVkC1lSNeMNBQhPG6jCEjXRh0uIUcLdSOSHXYqp4ni5SeoTWM")
     }
 
     private fun setAutoLogin() {
@@ -69,7 +71,7 @@ class LoginActivity : BaseActivity(), KeyboardVisibilityEventListener {
             if (hasFocus) {
                 rl_login_act_email.setBackgroundResource(R.drawable.primary_border)
                 img_login_act_email_off.setImageResource(R.drawable.login_id_on_icon)
-            } else if(!hasFocus && edt_login_act_email.text.isEmpty()){
+            } else if (!hasFocus && edt_login_act_email.text.isEmpty()) {
                 rl_login_act_email.setBackgroundResource(R.drawable.gray_border)
                 img_login_act_email_off.setImageResource(R.drawable.login_id_off_icon)
             }
@@ -78,7 +80,7 @@ class LoginActivity : BaseActivity(), KeyboardVisibilityEventListener {
             if (hasFocus) {
                 rl_login_act_password.setBackgroundResource(R.drawable.primary_border)
                 img_login_act_password_off.setImageResource(R.drawable.login_password_on_icon)
-            } else if(!hasFocus && edt_login_act_email.text.isEmpty()){
+            } else if (!hasFocus && edt_login_act_email.text.isEmpty()) {
                 rl_login_act_password.setBackgroundResource(R.drawable.gray_border)
                 img_login_act_password_off.setImageResource(R.drawable.login_password_off_icon)
             }
@@ -87,53 +89,69 @@ class LoginActivity : BaseActivity(), KeyboardVisibilityEventListener {
 
     private fun setOnBtnClickListener() {
         // 로그인 버튼 클릭 시
-        btn_login_act_submit.setOnClickListener {
-            val user_email = edt_login_act_email.text.toString()
-            val user_pw: String = edt_login_act_password.text.toString()
-            // 유효성 검사 후 로그인 통신
-            if (isValid(user_email, user_pw)) {
-                postLoginResponse(user_email, user_pw)
+        btn_login_act_submit.setOnClickListener(object : OnSingleClickListener() {
+            override fun onSingleClick(v: View) {
+                val user_email = edt_login_act_email.text.toString()
+                val user_pw: String = edt_login_act_password.text.toString()
+                // 유효성 검사 후 로그인 통신
+                if (isValid(user_email, user_pw)) {
+                    postLoginResponse(user_email, user_pw)
+                }
             }
-        }
+        })
+
         // 회원가입 클릭 시
-        txt_login_act_signup.setOnClickListener {
-            startActivity<SignupActivity>()
-        }
+        txt_login_act_signup.setOnClickListener(object : OnSingleClickListener() {
+            override fun onSingleClick(v: View) {
+                startActivity<SignupActivity>()
+
+            }
+        })
+
+        temp_login_act_btn.setOnClickListener(object : OnSingleClickListener() {
+            override fun onSingleClick(v: View) {
+                postLoginResponse("test1@naver.com", Integer.toString(123))
+            }
+        })
     }
 
     //리프레시 토큰
-    private fun refreshToken(refreshToken : String){
+    private fun refreshToken(refreshToken: String) {
         val getRefreshToken: Call<RefreshTokenResponse> =
-            networkService.getRefreshToken("application/json",
-                SharedPreferenceController.getAuthorization((this@LoginActivity)))
-        getRefreshToken.enqueue(object: Callback<RefreshTokenResponse>{
+            networkService.getRefreshToken(
+                "application/json",
+                SharedPreferenceController.getAuthorization((this@LoginActivity))
+            )
+        getRefreshToken.enqueue(object : Callback<RefreshTokenResponse> {
             override fun onFailure(call: Call<RefreshTokenResponse>, t: Throwable) {
                 Log.e("Refresh token failed", t.toString())
 
             }
 
             override fun onResponse(
-                call: Call<RefreshTokenResponse>, response: Response<RefreshTokenResponse>) {
+                call: Call<RefreshTokenResponse>, response: Response<RefreshTokenResponse>
+            ) {
                 Log.v("LoginActivity", response.toString())
 
                 Log.v("LoginActivity", response!!.body()!!.toString())
 
                 Log.v("LoginActivity", response!!.body()!!.message)
 
-                if(response.isSuccessful){
-                    if(response.body()!!.status == 200){
+                if (response.isSuccessful) {
+                    if (response.body()!!.status == 200) {
                         val tmp: RefreshToken = response!!.body()!!.data
                     }
                 }
             }
         })
     }
+
     private fun postLoginResponse(user_email: String, user_pw: String) {
 
         val postLoginResponse: Call<PostLoginResponse> =
             networkService.postLoginResponse("application/json", PostLoginRequest(user_email, user_pw))
-        postLoginResponse.enqueue(object: Callback<PostLoginResponse> {
-            override fun onFailure(call: Call<PostLoginResponse>, t: Throwable){
+        postLoginResponse.enqueue(object : Callback<PostLoginResponse> {
+            override fun onFailure(call: Call<PostLoginResponse>, t: Throwable) {
                 Log.e("Login failed", t.toString())
             }
 
@@ -174,8 +192,10 @@ class LoginActivity : BaseActivity(), KeyboardVisibilityEventListener {
         edt_login_act_email.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
             }
+
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
+
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 if (s!!.length != 0) {
                     rl_login_act_email.setBackgroundResource(R.drawable.primary_border)
@@ -192,8 +212,10 @@ class LoginActivity : BaseActivity(), KeyboardVisibilityEventListener {
         edt_login_act_password.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
             }
+
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
+
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 if (s!!.length != 0) {
                     rl_login_act_password.setBackgroundResource(R.drawable.primary_border)
@@ -205,6 +227,17 @@ class LoginActivity : BaseActivity(), KeyboardVisibilityEventListener {
             }
         })
     }
+
+    fun setUnderlineText() {
+        KeyboardVisibilityEvent.setEventListener(this, this)
+
+        val content = SpannableString(temp_login_act_tv.text.toString())
+
+        content.setSpan(UnderlineSpan(), 0, content.length, 0)
+
+        temp_login_act_tv.text = content
+    }
+
 
 }
 
