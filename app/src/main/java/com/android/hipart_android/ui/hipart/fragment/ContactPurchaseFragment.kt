5dbbec2bf877847e49d89723a089f866eb9554a1
vitriol2 <vitriol0919@gmail.com>
2019-07-13 05:45:51 +0900

    detail fragment packaging

---
+++ app/src/main/java/com/android/hipart_android/ui/hipart/fragment/ContactPurchaseFragment.kt
@@ -0,0 +1,106 @@
+package com.android.hipart_android.ui.hipart.fragment
+
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.android.hipart_android.R
+import com.android.hipart_android.network.ApplicationController
+import com.android.hipart_android.network.NetworkService
+import com.android.hipart_android.ui.hipart.data.GetHifiveNumResponse
+import com.android.hipart_android.util.Filter
+import kotlinx.android.synthetic.main.fragment_contact_purchase.*
+import org.jetbrains.anko.support.v4.toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class ContactPurchaseFragment : Fragment() {
+
+    val networkService: NetworkService by lazy{
+        ApplicationController.instance.networkService
+    }
+
+    private var nickname : String = ""
+    private var type : Int = 0
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        nickname = arguments!!.getString("nickname")
+        type = arguments!!.getInt("type")
+
+        return inflater.inflate(R.layout.fragment_contact_purchase, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+//
+//        setView(nickname, type)
+
+        getHifiveNumResponse("bj_ho")
+
+        setListeners()
+    }
+//
+//    private fun setView(nickname : String, type : Int) {
+//        tv_contact_purc_frag_name.text = nickname
+//        tv_contact_purc_frag_type.text = Filter.typePat(type)
+//    }
+
+    private fun setListeners() {
+        iv_contact_purc_frag_back.setOnClickListener {
+            activity!!.onBackPressed()
+        }
+        btn_contact_dial_frag_copy.setOnClickListener {
+            val number = tv_contact_purc_frag_nunmber.text
+            val cm = activity!!.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            cm.apply {
+                setPrimaryClip(ClipData.newPlainText(null, number))
+                toast("번호가 복사되었습니다")
+            }
+            activity!!.onBackPressed()
+        }
+    }
+
+    private fun getHifiveNumResponse(nickName:String) {
+        val getHifiveNumResponse = networkService.getHifiveNumResponse("application/json",
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuaWNrbmFtZSI6ImN1dGV5YW5nIiwiaWR4IjozLCJ0eXBlIjoxLCJpYXQiOjE1NjI1NjcyNTgsImV4cCI6MTU2Mzc3Njg1OCwiaXNzIjoiaWcifQ.WHzr5l6RfzF3Uw88qUeuJe9rpLD4RHlsCB9pto-4MbM"
+            //SharedPreferenceController.getAuthorization(this@ContactPurchaseFragment.context!!)
+            , nickName)
+        getHifiveNumResponse.enqueue(object : Callback<GetHifiveNumResponse> {
+
+            // 통신 실패 시
+            override fun onFailure(call: Call<GetHifiveNumResponse>, t: Throwable) {
+                Log.e("MyPageFragment Fail", t.toString())
+            }
+
+            // 통신 성공 시
+            override fun onResponse(call: Call<GetHifiveNumResponse>, response: Response<GetHifiveNumResponse>) {
+                if (response.isSuccessful) {
+                    Log.v("Success!", response.body()!!.toString())
+                    if (response.body()!!.message == "조회 성공") {
+                        Log.v("Success!", response.body()!!.toString())
+                        val data = response.body()!!.data
+
+                        tv_contact_purc_frag_farm.text = "남은 팜: " + data.point.toString() + "개"
+                        tv_contact_purc_frag_nunmber.text = data.number
+                        Log.d("ContactFragment", "number : ${data.number}")
+                    }
+                }
+            }
+        })
+    }
+
+
+}
---
+++ app/src/main/java/com/android/hipart_android/ui/hipart/fragment/HipartDetailTpatFragment.kt
@@ -0,0 +1,164 @@
+package com.android.hipart_android.ui.hipart.fragment
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.OrientationHelper
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.android.hipart_android.R
+import com.android.hipart_android.ui.hipart.HipartDetailActivity
+import com.android.hipart_android.ui.hipart.HipartDetailTagRecyclerAdapter
+import com.android.hipart_android.ui.hipart.HipartDetailTranslationAdapter
+import com.android.hipart_android.ui.hipart.HipartDetailTranslationData
+import com.android.hipart_android.ui.hipart.get.UserDetailTData
+
+
+import com.android.hipart_android.util.Filter
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_hipart_detail_eetc.*
+
+class HipartDetailTpatFragment : Fragment() {
+
+    private val TAG = "HipartDetailTFrag"
+
+    lateinit var user: UserDetailTData
+    var pickBtnFlag = true
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        user = arguments!!.getSerializable("user") as UserDetailTData
+        Log.d(TAG, user.user_nickname)
+        return inflater.inflate(R.layout.fragment_hipart_detail_eetc, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        setListeners()
+        setView()
+
+
+    }
+
+    private fun setView() {
+        ll_hip_det_eetc_frag_subscribe.visibility = View.GONE
+
+        Glide.with(activity!!)
+            .load(user.user_img)
+            .into(iv_frag_hip_det_eetc_photo)
+
+        tv_hip_det_frag_eetc_name.text = user.user_nickname
+        tv_hip_det_eetc_frag_pick_num.text = user.pick.toString()
+
+        tv_frag_hip_det_eetc_type.text = Filter.type(user.user_type)
+
+        if (user.pd != 0) {
+            tv_frag_hip_det_eetc_pd.text = Filter.pd(user.pd)
+        } else
+            rl_hip_det_eetc_frag_pd_background.visibility = View.GONE
+
+
+        setTagList(user)
+
+        tv_hip_det_eetc_frag_intro.text = user.detail_oneline
+        img_hipart_detail_work.setImageResource(R.drawable.hipat_tpat_work_adot_img)
+
+        setArticleList(user)
+
+        tv_frag_hip_det_eetc_hif_num.text =user.hifive.toString()
+
+        tv_frag_hip_det_eetc_want.text = user.detail_want
+        tv_frag_hip_det_eetc_spec.text = user.detail_appeal
+
+        //연락하기/연락처 보기
+        if (HipartDetailActivity.hifiveStatus == 1) {
+            btn_frag_hip_det_eetc_call.text = "연락처 보기"
+        } else
+            btn_frag_hip_det_eetc_call.text = "연락하기"
+    }
+
+    private fun setArticleList(user: UserDetailTData) {
+        val translationList = ArrayList<HipartDetailTranslationData>()
+
+        if (user.before!!.isNotEmpty()) {
+            ll_hipat_detail_frag_no_work.visibility = View.GONE
+            for (i in 0..user.before.size - 1)
+                translationList.add(
+                    HipartDetailTranslationData(
+                        user.before[i],
+                        user.after!![i]
+                    )
+                )
+            rv_frag_hip_det_frag_article.adapter =
+                HipartDetailTranslationAdapter(activity!!, translationList)
+        } else {
+            ll_hipat_detail_frag_no_work.visibility = View.VISIBLE
+        }
+
+        rv_frag_hip_det_frag_article.adapter = HipartDetailTranslationAdapter(activity!!, translationList)
+        rv_frag_hip_det_frag_article.layoutManager = LinearLayoutManager(activity!!, OrientationHelper.HORIZONTAL, false)
+    }
+
+    private fun setTagList(user: UserDetailTData) {
+        val tagList = ArrayList<String>()
+        if (user.concept != 0)
+            tagList.add(Filter.concept(user.concept))
+        if (user.lang != 0)
+            tagList.add(Filter.language(user.concept))
+        if (user.concept != 0)
+            tagList.add(Filter.etc(user.concept))
+
+        if (tagList.size > 0) {
+            rv_frag_hip_det_eetc_tag.adapter =
+                HipartDetailTagRecyclerAdapter(activity!!, tagList)
+            rv_frag_hip_det_eetc_tag.layoutManager =
+                LinearLayoutManager(activity!!, OrientationHelper.HORIZONTAL, false)
+        }
+    }
+
+    private fun setListeners() {
+        btn_frag_hip_det_eetc_call.setOnClickListener {
+            if(HipartDetailActivity.hifiveStatus == 0) {
+
+                val contactDialog = ContactDialogFragment()
+
+                val bundle = Bundle()
+                bundle.putString("nickname", user.user_nickname)
+                bundle.putInt("type", user.user_type)
+                contactDialog.arguments = bundle
+
+                contactDialog.show(childFragmentManager, "contact dialog")
+                Log.d(TAG, "contact btn clicked")
+            } else {
+                val purchaseFragment = ContactPurchaseFragment()
+
+                val bundle = Bundle()
+                bundle.putString("nickname", user.user_nickname)
+                bundle.putInt("type", user.user_type)
+                purchaseFragment.arguments = bundle
+
+                val fm = activity!!.supportFragmentManager
+                val fragmentTransaction = fm.beginTransaction()
+                fragmentTransaction.add(R.id.fl_hip_detail_act, purchaseFragment)
+                fragmentTransaction.addToBackStack(null)
+                fragmentTransaction.commit()
+            }
+        }
+        iv_frag_hip_det_eetc_back.setOnClickListener {
+            (context as HipartDetailActivity).finish()
+        }
+
+    }
+
+
+}
+
+
