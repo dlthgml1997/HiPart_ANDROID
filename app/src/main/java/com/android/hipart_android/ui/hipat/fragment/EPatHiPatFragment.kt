8b9df1b0641e063120a08ceea15a96d5a5a6dfd9
Tak Hyungmin <milkho11@naver.com>
2019-07-12 17:04:09 +0900

    *HipatFragment 에러 해결
    메인 중간 버튼으로 이동 시 데이터 삽입이 안되는 문제 해결

--- app/src/main/java/com/android/hipart_android/ui/hipat/fragment/EPatHiPatFragment.kt
+++ app/src/main/java/com/android/hipart_android/ui/hipat/fragment/EPatHiPatFragment.kt
@@ -3,13 +3,21 @@ package com.android.hipart_android.ui.hipat.fragment
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.android.hipart_android.R
+import com.android.hipart_android.network.ApplicationController
+import com.android.hipart_android.network.NetworkService
 import com.android.hipart_android.ui.hipat.adapter.PortFolioRecyclerViewAdapter
+import com.android.hipart_android.ui.hipat.data.GetProfileLookUpResponse
 import com.android.hipart_android.ui.mypick.data.GetMyPickData
+import com.android.hipart_android.util.SharedPreferenceController
 import kotlinx.android.synthetic.main.fragment_hipat_epat.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class EPatHiPatFragment : Fragment() {
 
@@ -44,38 +52,38 @@ class EPatHiPatFragment : Fragment() {
         rv_hipat_epat_frag.adapter = portFolioRecyclerViewAdapter
         rv_hipat_epat_frag.layoutManager = LinearLayoutManager(context!!, LinearLayoutManager.VERTICAL, false)
 
-      //   getProfileLookUp(2)
+         getProfileLookUp(2)
     }
 
-//    fun getProfileLookUp(flag : Int){
-//        var networkService: NetworkService = ApplicationController.instance.networkService
-//
-//        val getProfileLookUp = networkService.getProfileLookUp(
-//            SharedPreferenceController.getAuthorization(this@EPatHiPatFragment.context!!),
-//            flag
-//        )
-//
-//        getProfileLookUp.enqueue(object: Callback<GetProfileLookUpResponse> {
-//            override fun onFailure(call: Call<GetProfileLookUpResponse>, t: Throwable) {
-//                Log.e("All HighPat Frag Err", Log.getStackTraceString(t))
-//            }
-//
-//            override fun onResponse(
-//                call: Call<GetProfileLookUpResponse>,
-//                response: Response<GetProfileLookUpResponse>
-//            ) {
-//                response
-//                    ?.takeIf { it.isSuccessful }
-//                    ?.body()
-//                    ?.takeIf { it.message == "조회 성공" }
-//                    ?.data
-//                    ?.let {
-//                        portFolioRecyclerViewAdapter.dataList = it
-//                        portFolioRecyclerViewAdapter.notifyDataSetChanged()
-//                    }
-//            }
-//        })
-//    }
+    fun getProfileLookUp(flag : Int){
+        var networkService: NetworkService = ApplicationController.instance.networkService
+
+        val getProfileLookUp = networkService.getProfileLookUp(
+            SharedPreferenceController.getAuthorization(this@EPatHiPatFragment.context!!),
+            flag
+        )
+
+        getProfileLookUp.enqueue(object: Callback<GetProfileLookUpResponse> {
+            override fun onFailure(call: Call<GetProfileLookUpResponse>, t: Throwable) {
+                Log.e("All HighPat Frag Err", Log.getStackTraceString(t))
+            }
+
+            override fun onResponse(
+                call: Call<GetProfileLookUpResponse>,
+                response: Response<GetProfileLookUpResponse>
+            ) {
+                response
+                    ?.takeIf { it.isSuccessful }
+                    ?.body()
+                    ?.takeIf { it.message == "조회 성공" }
+                    ?.data
+                    ?.let {
+                        portFolioRecyclerViewAdapter.dataList = it
+                        portFolioRecyclerViewAdapter.notifyDataSetChanged()
+                    }
+            }
+        })
+    }
 
 //    fun setFilterData(flag: Int){
 //        when(flag){
